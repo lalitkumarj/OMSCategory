41f702534dc5711eac1cda61c10c98eeec08c847
Christelle Vincent <cvincent@stanford.edu>
2013-02-01 19:37:45 -0600

    commented out a bunch of imports in DistributionsSpace.py

--- SymkSpace.py
+++ SymkSpace.py
@@ -1,3 +1,18 @@
+import MSCoefficientModule
+#from sage.modules.module import Module
+from sage.structure.parent import Parent
+from sage.rings.padics.factory import ZpCA
+#from sage.rings.padics.padic_generic import pAdicGeneric
+#from sage.rings.rational_field import QQ
+from sage.rings.integer_ring import ZZ
+#from sage.misc.cachefunc import cached_method
+#from sage.categories.action import PrecomposedAction
+#from sage.structure.coerce_actions import LeftModuleAction, RightModuleAction
+#from sage.matrix.all import MatrixSpace
+#from sage.rings.fast_arith import prime_range
+from sage.modular.pollack_stevens.dist import get_dist_classes, Dist_long, iScale
+#import sage.rings.ring as ring
+
 class SymkSpace(Parent):
     def __init__(self, k, base, character, tuplegen, act_on_left):
         Parent.__init__(self,category=MSCoefficientModule, )
