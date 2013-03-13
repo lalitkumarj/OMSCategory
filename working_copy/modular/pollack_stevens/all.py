5bc5b479d8a02a13610e961a2df9c19edb65a863
Robert Harron <rharron@Robert-Harrons-MacBook-Pro.local>
2013-03-13 02:45:14 -0500

    Add imports to all.py to bring factory constructors into global namespace.

--- working_copy/modular/pollack_stevens/all.py
+++ working_copy/modular/pollack_stevens/all.py
@@ -1,5 +1,7 @@
-#from space import PSModularSymbols
-#from distributions import Distributions, Symk
+from modsym_OMS_space import OverconvergentModularSymbols
+from coeffmod_OMS_space import OverconvergentDistributions
+from modsym_OMS_families_space import FamiliesOfOMS
+from coeffmod_OMS_families_space import FamiliesOfOverconvergentDistributions
 from fund_domain import ManinRelations
 #from padic_lseries import pAdicLseries
 
