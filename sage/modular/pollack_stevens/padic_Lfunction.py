b3e7110fcc5cfe6473b60a2ddbf07531a7ff9881
Roberts Harron and Pollack <robert.harron@gmail.com>
2014-07-16 03:23:06 +0000

    Add a file for p-adic L-functions (padic_Lfunction.py)

---
+++ sage/modular/pollack_stevens/padic_Lfunction.py
@@ -0,0 +1,12 @@
+from sage.rings.power_series_ring import PowerSeriesRing
+
+class padic_Lfunction(SageObject):
+    pass
+
+class padic_Lfunction_one_variable(padic_Lfunction):
+    def __init__(self, Phi):
+        pass
+
+class padic_Lfunction_two_variable(padic_Lfunction):
+    def __init__(self, Phis):
+        pass
