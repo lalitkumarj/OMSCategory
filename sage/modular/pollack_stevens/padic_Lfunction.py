1f67964a81ee7d33a5e01ca3f7ea62592a31411d
Roberts Harron and Pollack <robert.harron@gmail.com>
2015-05-30 01:21:34 +0000

    Quick and dirty code for basic integral function in p-adic L-function code

--- sage/modular/pollack_stevens/padic_Lfunction.py
+++ sage/modular/pollack_stevens/padic_Lfunction.py
@@ -1,5 +1,7 @@
 from sage.structure.sage_object import SageObject
 from sage.rings.power_series_ring import PowerSeriesRing
+from sage.modular.pollack_stevens.manin_map import M2Z
+from sage.modular.pollack_stevens.families_util import logpp_binom
 
 class padic_Lfunction(SageObject):
     pass
@@ -11,7 +13,7 @@ class padic_Lfunction_one_variable(padic_Lfunction):
 class padic_Lfunction_two_variable(padic_Lfunction):
     def __init__(self, Phis, var='T', prec=None):
         #TODO: prec: Default it to None would make us compute it.
-        self._Phis = Phis    #should we create a copy of Phis, in case Phis changes?
+        self._Phis = Phis    #should we create a copy of Phis, in case Phis changes? probably
         self._coefficient_ring = Phis.base_ring()
         self._base_ring = PowerSeriesRing(self._coefficient_ring, var)    #What precision?
     
@@ -26,8 +28,20 @@ class padic_Lfunction_two_variable(padic_Lfunction):
         return (self._base_ring.gens()[0], self._coefficient_ring.gens()[0])
     
     def _basic_integral(self, a, j):
+        #is this the negative of what we want?
         #if Phis is fixed for this p-adic L-function, we should make this method cached
-        pass
+        p = Phis.parent().prime()
+        Da = M2Z([1,a,0,p])
+        onDa = self(Da)
+        aminusat = a - self.parent().base_ring().base_ring().teichmuller(a)
+        try:
+            ap = self._ap
+        except:
+            ap = self.Tq_eigenvalue(p) #catch exception if not eigensymbol
+        ans = onDa.moment(0)
+        for r in range(1, j+1):
+            ans += binomial(j, r) * (aminusat ** (j - r)) * (p ** r) * onDa.moment(r)
+        return (~ap) * ans
     
     def coefficient(self, index, twist=None):
         r"""
